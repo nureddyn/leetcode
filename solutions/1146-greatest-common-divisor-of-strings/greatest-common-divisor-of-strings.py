10cc008673c73fc69b6eebb488473a79f3836fd1
Nureddyn Ballout <nureddynballout@gmail.com>
2026-04-28 14:57:18 -0400

    Time: 7 ms (9.99%) | Memory: 19.2 MB (71.72%) - LeetSync

--- solutions/1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.py
+++ solutions/1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.py
@@ -1,15 +1,7 @@
 class Solution:
     def gcdOfStrings(self, str1: str, str2: str) -> str:
-        if len(str1) == 0 or len(str2) == 0:
-            return ""
-        if len(str1) < len(str2):
-            small_str, large_str = str1, str2
-        else:
-            small_str, large_str = str2, str1
-        for i in range(len(small_str), 0, -1):
-            substr = small_str[0:i]
-            if (len(large_str) % len(substr) == 0
-            and large_str == substr * int(len(large_str)/len(substr))
-            and small_str == substr * int(len(small_str)/len(substr))):
-                return substr
-        return ""
+        result = ""
+        for i in range(1, len(str1)+1):
+            if str1[0:i]*(len(str1)//len(str1[0:i])) == str1 and str1[0:i]*(len(str2)//len(str1[0:i])) == str2:
+                result = str1[0:i]
+        return result
